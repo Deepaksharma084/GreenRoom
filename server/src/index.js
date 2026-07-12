2fa7da7c3e16b5b8bc8ad204fb3ddebf7c0a7165
Deepaksharma084 <dk3433045@gmail.com>
2026-07-12 21:51:32 +0530

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import path from "path";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { fileURLToPath } from 'url';
