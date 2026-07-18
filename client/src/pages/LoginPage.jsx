a997e95824ef632f9f9e95ad3bcf5e19ac0c0332
Deepaksharma084 <dk3433045@gmail.com>
2026-07-18 23:21:23 +0530

    Config tailwind

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -12,23 +12,10 @@ export default function LoginPage() {
 
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Login</button>
-            </form>
+        <div className="flex items-center justify-center min-h-screen bg-red-500">
+            <div>
+
+            </div>
         </div>
     );
 }
