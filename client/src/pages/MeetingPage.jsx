a7f328c3cad470406c1b7a916d2864f1e39c59fc
Deepaksharma084 <dk3433045@gmail.com>
2026-08-08 21:04:06 +0530

    add socket at client side

--- client/src/pages/MeetingPage.jsx
+++ client/src/pages/MeetingPage.jsx
@@ -1,8 +1,36 @@
-import { useParams } from 'react-router-dom'
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { io } from "socket.io-client";
 
 export default function MeetingPage() {
-    const { roomId } = useParams()
-    console.log('MeetingPage roomId:', roomId)
+
+    const { roomId } = useParams();
+
+    useEffect(() => {
+
+        const socket = io("http://localhost:5000", {
+            withCredentials: true
+        });
+
+        socket.on("connect", () => {
+
+            console.log("Connected:", socket.id);
+
+            socket.emit("join-room", roomId);
+
+        });
+
+        socket.on("user-joined", (data) => {
+
+            console.log("New user joined:", data.socketId);
+
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+
+    }, [roomId]);
 
     return (
         <div className="min-h-screen bg-[radial-gradient(circle_at_top,_rgba(167,243,208,0.85),_rgba(5,46,22,0.95))] px-4 py-10 text-emerald-950">
@@ -11,5 +39,5 @@ export default function MeetingPage() {
                 <p>Room ID: {roomId}</p>
             </div>
         </div>
-    )
+    );
 }
