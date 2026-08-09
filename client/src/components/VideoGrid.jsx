05068422ae1a2baa840e84bbbd49049233572b16
Deepaksharma084 <dk3433045@gmail.com>
2026-08-09 20:04:12 +0530

    add VideoGrid function in the VideoGrid and import and utilize VideoTitle function

---
+++ client/src/components/VideoGrid.jsx
@@ -0,0 +1,26 @@
+import VideoTile from "./VideoTile.jsx";
+
+export default function VideoGrid({
+    localStream,
+    remoteStreams
+}) {
+    return (
+        <div className="grid min-h-screen grid-cols-1 gap-4 bg-black p-4 md:grid-cols-2">
+
+            {localStream && (
+                <VideoTile
+                    stream={localStream}
+                    muted={true}
+                />
+            )}
+
+            {remoteStreams.map((stream, index) => (
+                <VideoTile
+                    key={index}
+                    stream={stream}
+                />
+            ))}
+
+        </div>
+    );
+}
